679d80b4a835b6fe379305bff80a7f7603c1fc1c
Lin Marc <marclin750@gmail.com>
2024-02-13 10:07:11 +0100

    Ajout du route connexion-polar

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React, { FunctionComponent } from 'react';
 import ExerciseList from './pages/exercise-list';
 import ExercisesDetail from './pages/exercise-detail';
+import ConnexionPolar from './pages/connexionPolar';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
 const App: FunctionComponent = () => {
@@ -17,6 +18,7 @@ const App: FunctionComponent = () => {
                     <Route exact path="/" component={ExerciseList} />
                     <Route exact path="/exercises" component={ExerciseList} />
                     <Route path="/exercises/:id" component={ExercisesDetail} />
+                    <Route exact path="/connexion-polar" component={ConnexionPolar} />
                 </Switch>
             </div>
         </Router>
--- src/pages/connexionPolar.tsx
+++ src/pages/connexionPolar.tsx
@@ -3,7 +3,7 @@ import React, { FunctionComponent } from "react";
 const ConnexionPolar: FunctionComponent = () => {
     return (
         <div>
-    
+            <h1>Polar page</h1>
         </div>
     )
 }
